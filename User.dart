aa8079b48ac606b5d611e665d459445cdb76d8bd
Wison05 <92438146+Wison05@users.noreply.github.com>
2023-03-10 12:26:58 +0900

    Add files via upload
    처음으로 올린파일

---
+++ User.dart
@@ -0,0 +1,67 @@
+import 'dart:math';
+
+int getNum(int min, int max) {
+  Random r = new Random();
+  return min + r.nextInt(max-min);
+}
+
+class Unit{
+  int xp = 0; //경험치
+
+  void getXp(int _xp){
+    xp = xp + _xp;
+  }
+
+}
+
+// class abstract AttackType{
+//
+//   void punch();
+//
+//   void kick();
+// }
+
+// class Normal{
+//   void hello(){
+//     print("hello");
+//   }
+// }
+
+//플레이어 클래스 정의
+class User extends Unit{
+
+  //속성
+  String name; //이름
+  int hp; //체력
+  int minDmg; //최소데미지
+  int maxDmg; //최대데미지
+  int def; //방어력
+  late int xp;
+
+  User(this.name, this.hp, this.minDmg, this.maxDmg, this.def);
+
+  //기능
+  //die
+    /*bool isDead(){
+      if(hp <= 0) return true;
+      else return false;
+      }
+    */
+  bool isDead() => hp<=0? true:false;
+  // => : return
+
+  //attack
+  int attack(){
+    super.getXp(10);
+    xp = super.xp;
+    return getNum(minDmg, maxDmg);
+  }
+
+  //dmg
+  void getDmg(int dmg){
+    //데미지에서 방어력만큼 차감
+    dmg -= def;
+    if (dmg < 0) dmg = 0;
+    hp -= dmg;
+  }
+}
